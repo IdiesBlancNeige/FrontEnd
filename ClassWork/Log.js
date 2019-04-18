08abf35b31e2d79e29b3c406791a96ccaea010d6
Rostislav Rosaviktsy <rrosaviktsy@playtika.com>
2019-04-18 20:05:02 +0300

    Weather

--- ClassWork/Log.js
+++ ClassWork/Log.js
@@ -1,8 +1,27 @@
-var row = 10;
-var col = 15;
-for(var j=0;j<row;j++){
-    for(var i=0;i<col;i++){
-        console.log("x")
-    }
+const limit = 200 ;
+const Tax_Rates = 0.08 ;
+const Accessory_Prise = 7.99 ;
+var expences = 0;
+var balance = 5000;
+var wasted = 0 ;
+
+function getRandom(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
 }
-console.log("x");
+
+function calculated(phone, tax, accessory){
+    return (phone*tax)+phone+accessory+(accessory*tax);
+}
+
+while(limit >= wasted ){
+    const Phone_Prise = getRandom(100, 1000);
+    var Full_Prise = calculated(Phone_Prise, Tax_Rates, Accessory_Prise);
+    if(limit < Full_Prise){
+        break;
+    }
+    balance = balance - calculated(Phone_Prise, Tax_Rates, Accessory_Prise);
+    wasted = wasted + calculated(Phone_Prise, Tax_Rates, Accessory_Prise);
+    console.log(`Congratulations! Phone Prise: ${Phone_Prise} Full Prise: ${Full_Prise}`);
+}
+console.log('----------------------------------------------------------------');
+console.log(`Remaining balance ${balance} USD`);
